6e9240427944261a4f1f4169f94f91713389f367
syab-syab <syabsyabsyabsyab@gmail.com>
2023-07-24 21:44:29 +0900

    redux-test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import { decrement, increment, incrementByAmount } from './redux/counterSlice';
+import { useState } from 'react';
+// import Header from './components/Header';
+// import Main from './components/Main';
+// import Footer from './components/Footer';
 
 function App() {
+  // sotore.jsの中にあるcounterの値→counterSliceの中のvalueの値のこと
+  const count = useSelector((state) => state.counter.value);
+
+  const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState("2");
   return (
-    <div className="
-      App
-      text-center
-      
-      border
-      border-black
-      border-dashed
-      bg-orange-100
-    ">
-      <Header />
+    <div className="App">
+      {/* <Header />
       <Main />
-      <Footer />
+      <Footer /> */}
+      <h1>count: {count}</h1>
+      <input onChange={(e) => setIncrementAmount(e.target.value)} value={incrementAmount} />
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
+
     </div>
   );
 }
