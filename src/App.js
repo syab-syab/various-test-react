3171c718ae1960d0faf010463e0522871606ac2e
syab-syab <syabsyabsyabsyab@gmail.com>
2023-07-26 21:27:20 +0900

    メモ追加

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
   // store.jsのreducerに設定したオブジェクトのプロパティのcounterに対応する
   const count = useSelector((state) => state.counter.count);
 
+
   // countの値をイジるにはAction creatorsを呼び出して
   // それを呼び出すためにuseDispatchが必要になる
   const dispatch = useDispatch();
+
 
   return (
     <div className="App">
@@ -21,6 +23,7 @@ function App() {
       {/* それをimportして使う */}
       <button onClick={() => dispatch(increase())}>Up</button>
       <button onClick={() => dispatch(decrease())}>Down</button>
+
 
     </div>
   );
--- src/redux/counterSlice.js
+++ src/redux/counterSlice.js
@@ -1,13 +1,17 @@
 // **Slice.jsと名付けるのが慣例になってるっぽい？
 
 import { createSlice } from "@reduxjs/toolkit";
+import numValue from "../other/data.json";
 
 export const counterSlice = createSlice({
     // name=Sliceを識別するための名前
     name: 'counter',
     // initialState=共有するデータ(state)の初期値を設定
     initialState: {
-        count: 0,
+        // こんな感じでjson形式のファイルも入れられる
+        // あくまで状態管理だけだから外部リソースへのアクセスはカスタムフックの方が無難っぽい
+        // ここに入れるのは外部リソースのデータを選ぶためのidとかの方が良い
+        count: numValue["value"][0]["value"]
     },
     // reducers=stateを更新するための関数を設定
     reducers: {
